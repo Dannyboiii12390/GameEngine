d148a37ce6806d2ec3de7182ade6529f54715aa2
Dannyboiii12390 <djhowe18@gmail.com>
2026-02-10 09:10:42 +0000

    changed vcproj version so it works on vs2022

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -61,7 +61,7 @@ int main()
 		Physics::Sphere testSphere(glm::vec3(0.0f, 0.0f, 0.0f), 1.0f);
 		Physics::Sphere testSphere2(glm::vec3(0.5f, 0.0f, 0.0f), 1.0f);
 
-		std::cout << "Colling? " << (testSphere.isCollidingWith(testSphere2) ? "Yes" : "No") << std::endl;
+		std::cout << "Colliding? " << (testSphere.isCollidingWith(testSphere2) ? "Yes" : "No") << std::endl;
 
 
         
