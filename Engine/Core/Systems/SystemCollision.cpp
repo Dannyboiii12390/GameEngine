01e2f89607027001d15c99f32c5bc08e83550e0e
Dannyboiii12390 <djhowe18@gmail.com>
2026-03-11 12:14:41 +0000

    balls sticking together

--- Engine/Core/Systems/SystemCollision.cpp
+++ Engine/Core/Systems/SystemCollision.cpp
@@ -7,35 +7,8 @@
 
 void SystemCollision::OnUpdate(std::span<Entity> entities, float deltaTime)
 {
-	auto checkCollisionForEntity = [&entities](Entity& entity)
-	{
-			EComponentType requiredComponents = EComponentType::Component_Collision | EComponentType::Component_Transform;
-		if (!entity.HasComponent(requiredComponents))
-		{
-			return;
-		}
-		ComponentCollision* collisionComp = entity.GetComponent<ComponentCollision>(EComponentType::Component_Collision);
-		ComponentTransform* thisTransform = entity.GetComponent<ComponentTransform>(EComponentType::Component_Transform);
-		auto* collider = collisionComp->GetCollider();
-		collider->setPosition(thisTransform->Position());
-		collider->setRotation(thisTransform->Rotation());
-		for(Entity& other : entities)
-		{
-			if (&entity == &other) continue; // skip self
-			if (!other.HasComponent(EComponentType::Component_Collision))
-			{
-				continue;
-			}
-			ComponentCollision* otherCollision = other.GetComponent<ComponentCollision>(EComponentType::Component_Collision);
-			bool isColliding = collisionComp->Collided(*otherCollision->GetCollider());
-			if(isColliding)
-			{
-				collisionComp->InvokeCollision(entity, other);
-			}
-		}
-	};
-	std::for_each(std::execution::par_unseq, entities.begin(), entities.end(), checkCollisionForEntity);
-	/*
+	EComponentType requiredComponents = EComponentType::Component_Collision | EComponentType::Component_Transform;
+
 	for (Entity& entity : entities)
 	{
 		if (!entity.HasComponent(requiredComponents))
@@ -74,6 +47,6 @@ void SystemCollision::OnUpdate(std::span<Entity> entities, float deltaTime)
 			}
 		}
 	}
-	*/
+	
 }
 
